ebe5a71d0b24ab150a0319c3736837341ac32cb7
ruirchik@mail.ru <ruirchik@mail.ru>
2021-09-08 09:27:13 +0300

    правки

--- source/js/modules/promo-slider.js
+++ source/js/modules/promo-slider.js
@@ -3,5 +3,25 @@ const promoSwiper = new Swiper('.promo__slider', {
   spaceBetween: 16,
   navigation: {
     nextEl: ".promo__next",
+    prevEl: ".promo__prev",
   },
 });
+
+const card = document.querySelector('.promo__card');
+let topCard = card.getBoundingClientRect().top;
+let leftCard = card.getBoundingClientRect().left;
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > topCard) {
+    card.style.position = 'fixed';
+    card.style.left = leftCard + 'px';
+    card.style.top = 0;
+    card.style.margin = 0;
+  } else {
+    card.style = "";
+  }
+});
+
+window.addEventListener('resize', function(){
+  topCard = card.getBoundingClientRect().top;
+  leftCard = card.getBoundingClientRect().left;
+})
