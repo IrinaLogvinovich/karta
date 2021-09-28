4dee70ec67323dba0e1759f3de17186e520531a5
Irina <ruirchik@mail.ru>
2021-09-28 05:37:57 +0300

    забытый коммит

---
+++ source/js/modules/items.js
@@ -0,0 +1,9 @@
+const itemsSwiper = new Swiper('.items__container', {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
